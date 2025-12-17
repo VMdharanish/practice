94eda512c4023cb2dfdd72dbdf8ff97dc6b75fbb
V M Dharanish <2kdharanish@gmail.com>
2025-12-17 23:05:13 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -4,4 +4,18 @@ def reverse(arr):
         arr[left],arr[right]=arr[right],arr[left]
     return arr
 
+#Remove duplicate values
+
+def remove_duplicates(arr):
+    if not arr:
+        return 0
+    s = 0
+    for i in range(1,len(arr)):
+        if arr[s] != arr[i]:
+            arr[s],arr[i] = arr[i],arr[s]
+            s+=1
+    return s+1
+arr = [1, 1, 2, 2, 3]
+k = remove_duplicates(arr)
+print(k, arr[:k])
 
