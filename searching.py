1e16a4fc6546f6bcb954c4adda519d45d3853976
V M Dharanish <2kdharanish@gmail.com>
2025-12-23 21:44:40 +0530

    Update searching.py

--- searching.py
+++ searching.py
@@ -1,3 +1,4 @@
+"""Searching techinque using menuj bar method"""
 '''linear seaching'''
 class linear:
     def lin(self,arr,key):
@@ -6,10 +7,40 @@ class linear:
             if arr[i] == key:
                 return 1
         return -1
-obj = linear()
-arr = [1,2,3,4,7,4,6,8,9,12,34]
-key = 2
-if obj.lin(arr,key):
-    print(True)
+
+'''BInary seaching'''
+class binary:
+    def bina(self,arr,key):
+        low,high=0,len(arr)
+        while low<=high:
+            mid = (low + high) //2
+            if arr[mid] == key:
+                return mid
+            elif arr[mid] < key:
+                low = mid + 1
+            else:
+                high = arr[mid] - 1
+arr = list(input("Enter arr : "))
+key = int(input("Enter key : "))
+cc = int(input("1:linear search\n2:Binary search\n choicce : "))
+if cc == 1:
+    obj = linear()
+    if obj.lin(arr,key):
+        print(True)
+    else:
+        print(False)
 else:
-    print(False)
+    obj = binary()
+    if obj.bina(arr,key):
+        print(True)
+    else:
+        print(False)
+
+OUTPUT
+
+Enter arr : [1,2,3,4,7,4,6,8,9,12,34]
+Enter key : 7
+1:linear search
+2:Binary search
+ choicce : 1
+True
