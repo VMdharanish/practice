b7685c4ded5ccb94b8460626a2e3c3fa4861a70c
V M Dharanish <2kdharanish@gmail.com>
2026-01-04 20:44:27 +0530

    stock_buy_and_sell 2.py

---
+++ stock_buy_and_sell 2.py
@@ -0,0 +1,20 @@
+"""Given an array prices[] of length n, representing the prices of the stocks on different days. The task is to find the maximum profit possible by
+buying and selling the stocks on different days when at most one transaction is allowed. Here one transaction means 1 buy + 1 Sell. If it is not possible to make a profit then return 0.
+
+Note: Stock must be bought before being sold.
+
+Examples:
+
+Input: prices[] = [7, 10, 1, 3, 6, 9, 2]
+Output: 8
+Explanation: You can buy the stock on day 2 at price = 1 and sell it on day 5 at price = 9. Hence, the profit is 8."""
+
+class Solution:
+    def maximumProfit(self, prices):
+        # code here
+        small = prices[0]
+        result = 0
+        for i in range(1,len(prices)):
+            small = min(small,prices[i])
+            result = max(result,prices[i] - small)
+        return result
