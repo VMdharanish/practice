2cfb94355ad8222ecb65fbe392e2c5dbaefbec08
V M Dharanish <2kdharanish@gmail.com>
2026-03-12 23:22:02 +0530

    Create C2C.java

---
+++ C2C.java
@@ -0,0 +1,8 @@
+importt java.util.Scanner;
+public class C2C{
+  public static void maiin(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String Nmae = sc.next();
+    int Age = sc.nextInt();
+    String Gender = sc.next();
+    System.out.println("Name : "+Name+"Age : "+Age+"Gender : "+Gender.ChatAt(0));
