2e818c781ba6c9b300c6bcbe96bc67e17116e156
V M Dharanish <2kdharanish@gmail.com>
2026-03-31 23:29:49 +0530

    Create typecasting.java

---
+++ typecasting.java
@@ -0,0 +1,14 @@
+class testing{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        char a = 'h';
+        int b =  Character.getNumericValue(a);
+        System.out.println(b);
+        int c = 65;
+        char d = (char) c;
+        System.out.println(d);
+        int e = 150;
+        byte f = (byte)e;
+        System.out.println(f);
+    }   
+}
