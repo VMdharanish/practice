45981d58e5c2355219331a0205bbaf6cc0f7a3bf
V M Dharanish <2kdharanish@gmail.com>
2026-04-01 23:43:10 +0530

    Add basic arithmetic operations in Java

---
+++ arithematic_operator.java
@@ -0,0 +1,16 @@
+class arithematic_operator{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        int sub=a-b;
+        int mul=a*b;
+        int div=a/b;
+        System.out.println("sum of a and b is: "+sum);
+        System.out.println("sub of a and b is: "+sub);
+        System.out.println("mul of a and b is: "+mul);
+        System.out.println("div of a and b is: "+div);
+        int ab =(int)a+a;
+        System.out.println("a+a :"+ab);
+    }
+}
