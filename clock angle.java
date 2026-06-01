7c7481f97f631417f095e634b2c774bff526f128
V M Dharanish <2kdharanish@gmail.com>
2026-06-01 22:20:58 +0530

    Create clock angle.java

---
+++ clock angle.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static double clockAngle(String s) {
+        String[] parts = s.split(":");
+
+        int hour = Integer.parseInt(parts[0]);
+        int minute = Integer.parseInt(parts[1]);
+
+        hour %= 12;
+
+        double hourAngle = hour * 30 + minute * 0.5;
+        double minuteAngle = minute * 6;
+
+        double angle = Math.abs(hourAngle - minuteAngle);
+
+        return Math.min(angle, 360 - angle);
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("%.3f\n", clockAngle("06:00"));
+        System.out.printf("%.3f\n", clockAngle("03:15"));
+    }
+} 
